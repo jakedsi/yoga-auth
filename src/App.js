de03dbdafc81ceddd663ee806bf927cf60dc67b6
jakeaboyot <jakeaboyot0@gmail.com>
2022-10-19 15:14:49 +0800

    signup enable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home/Home'
 import Classes from './components/Classes/Classes'
 import About from './components/About/About'
 import Login from './components/Login/Login'
-// import Signup from './components/Signup/Signup'
+import Signup from './components/Signup/Signup'
 import Error from './components/Error/Error'
 // import Profile from './components/Profile/Profile'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
@@ -18,8 +18,8 @@ export default function App() {
           <Route exact path='/classes' element={<Classes />} />
           <Route exact path='/about' element={<About />} />
           <Route exact path='/login' element={<Login />} />
-          {/* <Route exact path='/signup' element={<Signup />} />
-          <Route exact path='/profile' element={<Profile />} />
+          <Route exact path='/signup' element={<Signup />} />
+          {/* <Route exact path='/profile' element={<Profile />} />
           <Route exact path='/profile/:username' element={<Profile />} /> */}
           <Route exact path='*' element={<Error />} />
         </Routes>
