7d076fb4bbe9209ae2dbda51cca4b9bb28930913
jakeaboyot <jakeaboyot0@gmail.com>
2022-10-19 19:00:13 +0800

    another bug

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,7 +39,7 @@ export default function Login() {
         //   })
         //   .catch(error => funcErr(error.response.data))
     }
-    // const withoutQuotes = err.replaceAll('"', '');
+    // const withoutQuotes = err.replaceAll('"', ''); just another bug
 
   return (
     <div className='not-home'>
