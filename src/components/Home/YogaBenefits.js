db1cfcf97b341f99f8e95c20207182e7dfe2fb35
jakeaboyot <jakeaboyot0@gmail.com>
2022-10-20 18:50:10 +0800

    yoga benefits responsive phone

--- src/components/Home/YogaBenefits.js
+++ src/components/Home/YogaBenefits.js
@@ -10,8 +10,8 @@ import twoLines from '../../assets/images/two-lines.svg'
 export default function YogaBenefits() {
   return (
     <>
-<section className="overflow-hidden text-pink-50 lg:flex md:block justify-center items-center xl:h-[95vh] lg:h-[80vh]">
-  <div className="container mx-auto lg:px-10 md:p-20 sm:p-10 lg:p-0" >
+<section className="overflow-hidden text-pink-50 lg:flex md:block justify-center items-center xl:h-[95vh] lg:h-[80vh] xs:mb-2">
+  <div className="container mx-auto lg:px-10 md:p-20 sm:p-10 lg:p-0 xs:p-5" >
     <div className="flex flex-wrap">
       <div className="flex flex-wrap w-1/2">
         <div className="w-full p-1 md:p-2">
@@ -35,15 +35,15 @@ export default function YogaBenefits() {
       </div>
     </div>
   </div>
-  <div className="container mx-auto lg:px-10 h-2/3 flex items-center md:px-24 sm:px-16">
+  <div className="container mx-auto lg:px-10 h-2/3 flex items-center md:px-24 sm:px-16 xs:px-9">
     <div className='just-text 2xl:pl-20 2xl:pr-32 xl:pl-0 lg:pr-0 '>
-           <img src={benefitsDots} alt="" className='ml-auto lg:hidden xl:block md:hidden sm:hidden'/> <br></br>
-           <h1 className='border-l-2 pl-2 text-[34px] font-bold'>Yoga<span className='b-text text-[19px] font-medium tracking-widest'> Benefits</span></h1>
-           <p className='mt-2 font-light xl:mb-0 lg:mb-5 md:mb-5 sm:mb-3'>Regular yoga practice may reduce levels of stress and body-wide inflammation, contributing to healthier hearts. Several of the factors contributing to heart disease, including high blood pressure and excess weight, can also be addressed through yoga.</p>
+           <img src={benefitsDots} alt="" className='ml-auto lg:hidden xl:block hidden'/> <br></br>
+           <h1 className='xs:text-center xs:border-l-0 sm:text-left sm:border-l-2 pl-2 text-[34px] font-bold'>Yoga<span className='b-text text-[19px] font-medium tracking-widest'> Benefits</span></h1>
+           <p className='mt-2 font-light xl:mb-0 lg:mb-5 md:mb-5 sm:mb-3 xs:text-[11px] sm:text-[17px] sm:text-left xs:text-center'>Regular yoga practice may reduce levels of stress and body-wide inflammation, contributing to healthier hearts. Several of the factors contributing to heart disease, including high blood pressure and excess weight, can also be addressed through yoga.</p>
            <div className='flex items-center justify-center'>
-            <img src={benefitsArrow} alt="" className='lg:hidden xl:block md:hidden sm:hidden'/>
-            <button className='mt-1 bg-amber-900 py-1 rounded-[13px] 2xl:px-4 2xl:ml-3 2xl:w-[20vw] xl:w-[36vw] xl:ml-3 lg:w-[100%] md:w-[100%] lg:ml-0 sm:w-[50%]'>Read More</button>
-            <img src={twoLines} alt="" className='float-right lg:hidden md:hidden sm:hidden xl:block'/>
+            <img src={benefitsArrow} alt="" className='xl:block hidden'/>
+            <button className='sm:mt-1 bg-amber-900 py-1 rounded-[13px] 2xl:px-4 2xl:ml-3 2xl:w-[20vw] xl:w-[36vw] xl:ml-3 lg:w-[100%] md:w-[100%] lg:ml-0 sm:w-[50%] xs:w-[50%] xs:mt-3'>Read More</button>
+            <img src={twoLines} alt="" className='float-right hidden xl:block'/>
           </div>
     </div>
   </div>
